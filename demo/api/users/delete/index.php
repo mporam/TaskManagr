c673a962e8f75b480886760824670d324a462f51
mporam <emailme@mikeoram.co.uk>
2014-03-30 19:39:11 +0100

    Fix api save/delete files and create save test file

--- demo/api/users/delete/index.php
+++ demo/api/users/delete/index.php
@@ -1,12 +1,12 @@
 <?php
 if (!empty($_POST['users_id'])) {
 	require($_SERVER['DOCUMENT_ROOT'] . '/includes/sql/db_con.php');
-        require($_SERVER['DOCUMENT_ROOT'] . '/includes/config.php');
+    require($_SERVER['DOCUMENT_ROOT'] . '/includes/config.php');
 
-        session_start();
+    session_start();
  	$id =  $_POST['users_id'];
-        $access = $_SESSION['user_type'];
-        if ($access !== '1') die(json_encode(array(message => 'Permissions Denied', code => 502)));
+    $access = $_SESSION['user_type'];
+    if ($access !== '1') die(json_encode(array(message => 'Permissions Denied', code => 502)));
 
 	$query = $con->prepare("DELETE FROM users WHERE `comments_id` = $id");
 
@@ -14,20 +14,20 @@ if (!empty($_POST['users_id'])) {
 		$query->execute();
 	} catch (PDOException $e) {
 		$result = array(
-			code => 500,
-			message => 'Delete Failed. Please try again.'
+			'code' => 500,
+			'message' => 'Delete Failed. Please try again.'
 		);
 		die(json_encode($result));
 	}
 	
 	$result = array(
-		code => 200,
-		message => 'User Permanently Deleted',
-		id => $id
+		'code' => 200,
+		'message' => 'User Permanently Deleted',
+		'id' => $id
 	);
 	
 	echo json_encode($result);
 
 } else {
-	die(json_encode(array(message => 'Internal Server Error', code => 500)));
+	die(json_encode(array('message' => 'Internal Server Error', 'code' => 500)));
 }
--- demo/api/users/save/index.php
+++ demo/api/users/save/index.php
@@ -1,51 +1,50 @@
 <?php
 if (!empty($_POST)) {
 	require($_SERVER['DOCUMENT_ROOT'] . '/includes/sql/db_con.php');
-        require($_SERVER['DOCUMENT_ROOT'] . '/includes/config.php');
+    require($_SERVER['DOCUMENT_ROOT'] . '/includes/config.php');
 
 	$name =  trim($_POST['users_name']);
-        $email =  $_POST['users_email'];
-        $password =  (empty($_POST['users_password']) ? '0' : $_POST['users_password']);
+    $email =  $_POST['users_email'];
+    $password =  (empty($_POST['users_password']) ? '0' : $_POST['users_password']);
  	$type =  $_POST['users_type'];
 
 	if (!empty($_POST['users_id'])) {
 		$id = $_POST['users_id'];
-                $salt = $_POST['users_salt'];
-                $password = sha1($password . $salt);
+        $salt = $_POST['users_salt'];
+        $password = sha1($password . $salt);
 
 		$query = $con->prepare("UPDATE users SET `users_name`='$name', `users_email`='$email', `users_password`='$password', `users_type`=$type WHERE `users_id` = $id");
 	} else {
-                $salt = rand(0, 9999);
-                $password = sha1($password . $salt);
-
-		$query = $con->prepare("INSERT INTO users (`users_name`, `users_email`, `users_password`, `users_type`) VALUES ('$name', '$email', '$password', $type)");
+        $salt = rand(0, 9999);
+        if ($password) $password = sha1($password . $salt);
+		$query = $con->prepare("INSERT INTO users (`users_name`, `users_email`, `users_password`, `users_salt`, `users_type`) VALUES ('$name', '$email', '$password', '$salt', $type)");
 	}
 
 	try {	
 		$query->execute();
 	} catch (PDOException $e) {
 		$result = array(
-			code => 500,
-			message => 'Save Failed. Please try again.'
+			'code' => 500,
+			'message' => 'Save Failed. Please try again.'
 		);
 		die(json_encode($result));
 	}
 	
 	if (empty($_POST['users_id'])) {
 		$lastid = $con->lastInsertId();
-                $message = "User Created";
+        $message = "User Created";
 	} else {
 		$lastid = $_POST['users_id'];
-                $message = "User Updated";
+        $message = "User Updated";
 	}
 	$result = array(
-		code => 200,
-		message => $message,
-		id => $lastid
+		'code' => 200,
+		'message' => $message,
+		'id' => $lastid
 	);
 	
 	echo json_encode($result);
 
 } else {
-	die(json_encode(array(message => 'Internal Server Error', code => 500)));
+	die(json_encode(array('message' => 'Internal Server Error', 'code' => 500)));
 }
