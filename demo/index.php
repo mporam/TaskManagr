4d8f900a3a8f08e24977815b184762896b3b1b47
maximus-lynn <maxlynn12@gmail.com>
2014-04-15 20:19:23 +0100

    [Taskmangr] - Grid updates

--- demo/index.php
+++ demo/index.php
@@ -5,4 +5,4 @@
 <body>
 <?php var_dump($_SESSION); ?>
     
-<?php require($_SERVER['DOCUMENT_ROOT'] . '/includes/template/bottom.php'); ?>
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/includes/template/bottom.php'); ?> 
