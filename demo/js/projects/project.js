eb825ccc6178d4eae04d3725cb129fadb1f21524
mporam <emailme@mikeoram.co.uk>
2014-04-24 00:14:25 +0100

    commented out pagination stuff for time being

--- demo/js/projects/project.js
+++ demo/js/projects/project.js
@@ -12,17 +12,19 @@ var project, tasks
             
             var table = $('<table></table>');
             
+            /* ---------- this is for pagination if we want it. DO NOT TOUCH!
             var limit = {};
-            limit.end = "5";
+            limit.end = "20";
             if (window.location.hash.substr(2).length > 0) {
-                limit.end = window.location.hash.substr(2) * 5;
+                limit.end = window.location.hash.substr(2) * 20;
             }
             limit.start = limit.end - 5;
+            */
             
             $.ajax({
                 type: "POST",
                 url: '/api/tasks/',
-                data: {"projects_id":project.projects_id, "limit": limit.start + ', ' + limit.end},
+                data: {"projects_id":project.projects_id}, //, "limit": limit.start + ', ' + limit.end},
                 success: function(data) {
                     tasks = data;
                     tasks.forEach(function(task) {
