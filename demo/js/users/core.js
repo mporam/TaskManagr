0e96f845d9efffb71e6959a62bb0252587e2825e
mporam <emailme@mikeoram.co.uk>
2014-05-05 10:55:48 +0100

    create user profile pages

--- demo/js/users/core.js
+++ demo/js/users/core.js
@@ -11,9 +11,9 @@ $(function() {
                 $('body').trigger('complete');
                 users.forEach(function(user) {
                     var row = '<tr>';
-                    row += '<td><a href="/users/?name=' + user.users_name + '">' + user.users_name + '</a></td>';
+                    row += '<td><a href="/users/user/?name=' + user.users_name + '">' + user.users_name + '</a></td>';
                     row += '<td>' + user.users_email + '</td>';
-                    row += '<td>' + user.users_type + '</td>';
+                    row += '<td>' + user.users_type_name + '</td>';
                     row += '<td><img src="' + user.users_image + '"></td>';
                     row += '</tr>';
                     $('#users table tbody').append(row);
