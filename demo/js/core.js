02ac8c43395034058493cee14dd4268fca071b30
mporam <emailme@mikeoram.co.uk>
2014-05-05 18:42:49 +0100

    create sidebar and functions to open/close it

--- demo/js/core.js
+++ demo/js/core.js
@@ -1,4 +1,29 @@
-var hash;
+var hash, sidebar;
 if (window.location.hash == "") {
     window.location.hash = '/';
+}
+$(function() {
+    sidebar = $('#sidebar');
+});
+
+var openSidebar = function() {
+    sidebar.removeClass('closed');
+    sidebar.animate(
+        {width: "20%"},
+        2000,
+        function() {
+            sidebar.addClass('open');
+        }
+    );
+}
+
+var closeSidebar = function() {
+    sidebar.removeClass('open');
+    sidebar.animate(
+        {width: "0"},
+        2000,
+        function() {
+            sidebar.addClass('closed');
+        }
+    );
 }
