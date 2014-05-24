82905ca5c7b2fca4cc7924aad2b7f9b1907c4adb
mporam <emailme@mikeoram.co.uk>
2014-05-24 12:07:03 +0100

    create production, staging and local environments

--- demo/includes/sql/db_con.php
+++ demo/includes/sql/db_con.php
@@ -1,9 +1,19 @@
 <?php
-/* Connect to an ODBC database using driver invocation */
+$env = 'local';
 if ($_SERVER['SERVER_NAME'] == 'demo.taskmanagr.co.uk') {
+    $env = 'production';
+} else if ($_SERVER['SERVER_NAME'] == 'staging.taskmanagr.co.uk') {
+    $env = 'staging';
+}
+
+/* Connect to an ODBC database using driver invocation */
+if ($env == 'production') {
 	// turn off error reporting 
-	//error_reporting(0);
+	error_reporting(0);
 	$dsn = 'mysql:dbname=taskmana_db;host=10.168.1.50';
+} else if ($env == 'staging') {
+    error_reporting(E_ALL);
+    $dsn = 'mysql:dbname=taskmana_db;host=10.168.1.50';
 } else {
 	$dsn = 'mysql:dbname=taskmana_db;host=91.208.99.2:3350';
 }
