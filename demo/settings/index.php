97fe2c07a2bca937e938fa2ff5d8fd2204c47317
mporam <emailme@mikeoram.co.uk>
2014-09-25 17:50:17 +0100

    add settings page

---
+++ demo/settings/index.php
@@ -0,0 +1,9 @@
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/includes/template/top.php'); ?>
+    <title>Admin</title>
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/includes/template/header.php'); ?>
+
+    <div id="settings">
+        <h2>Settings</h2>
+    </div>
+
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/includes/template/bottom.php'); ?>
